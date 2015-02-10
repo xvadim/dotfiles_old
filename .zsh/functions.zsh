72ded1084e14a77466147ee5375b416bec48b2ee
Vadim Khohlov <vadim.khohlov@postindustria.com>
2015-02-10 14:01:34 +0200

    Added function for archive unpacking

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -1,3 +1,4 @@
+# shortcut for git. Without args - git status
 function g {
   if [[ $# > 0 ]]; then
     git $@
@@ -6,8 +7,31 @@ function g {
   fi
 }
 
+# make and cd to a dir
 function take {
   mkdir $1
   cd $1
 }
 
+# example: extract file
+extr () {
+    if [ -f $1 ] ; then
+        case $1 in
+            *.tar.bz2)   tar xjf $1 ;;
+            *.tar.gz)    tar xzf $1 ;;
+            *.bz2)       bunzip2 $1 ;;
+            *.rar)       unrar x $1 ;;
+            *.gz)        gunzip $1  ;;
+            *.tar)       tar xf $1  ;;
+            *.tbz2)      tar xjf $1 ;;
+            *.tbz)       tar -xjvf $1;;
+            *.tgz)       tar xzf $1 ;;
+            *.zip)       unzip $1   ;;
+            *.Z)         uncompress $1;;
+            *.7z)        7z x $1    ;;
+            *)           echo "I don't know how to extract '$1'..." ;;
+        esac
+    else
+        echo "'$1' is not a valid file"
+    fi
+}
